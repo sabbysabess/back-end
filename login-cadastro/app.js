5cddf75d1fb0c56450395073f51ee1e05bcc2e0c
sabbysabess <124396604+sabbysabess@users.noreply.github.com>
2024-05-23 15:03:46 -0300

    inserindo o login cadastro

---
+++ login-cadastro/app.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mysql = require('mysql2');
+
+const app = express();
+const port = 3000;
+
+const db = mysql.createConnection({
+    host:'localhost',
+    user: 'sabe',
+    password:'senai123',
+    database: 'login'
+});
+
+db.connect((error)=>{
+    if (error){
+        console.log("Erro ao conectar com o banco de dados")
+    } else {
+        console.log("Conectado ao MySQL")
+    }
+});
+
+app.use(bodyParser.urlencoded({extended:true}))
+
+app.get("/", (req, res)=>{
+    res.sendFile(__dirname + '/login.html')
+});
+
+app.post('/login', (req, res) => {
+    const { username, password } = req.body;
+  
+    db.query('SELECT password FROM user WHERE username = ?', [username], (error, results) => {
+      if(error){
+        res.status(500).send('Erro ao obter usuários')
+      } else {
+        if(results.length > 0){ // Verifica se há resultados
+          const user = results[0]; // Obtém o primeiro resultado
+          if(user.password === password){
+             res.send(`Login bem-sucedido! Bem-vindo, ${username}.`);
+          } else {
+            res.status(401).send('Senha incorreta. Tente novamente.');
+          }
+        } else {
+          res.status(401).send('Este usuário não existe');
+        }
+      }
+    })
+  });
+app.listen(port, ()=>{
+    console.log(`Servidor rodando no endereço: http://localhost:${port}`)
+});
+
---
+++ login-cadastro/db/login.sql
@@ -0,0 +1,18 @@
+create database login;
+use login;
+
+create table user (
+idUser int primary key,
+username varchar(20) not null,
+password varchar(8) not null
+);
+
+insert into user (idUSer, username, password) values 
+(1, 'sabesabes', 'miojo456');
+select * from user;
+
+insert into user (idUSer, username, password) values 
+(2, 'mari', 'abcd');
+
+insert into user (idUser, username, password) values 
+(3, 'joao', 'teste');
